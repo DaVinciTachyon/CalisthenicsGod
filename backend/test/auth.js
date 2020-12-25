31e35714bdbee3a2288fb8aeaf889d754208f7e3
Michael McGuinness <michael.mcguinness@utmost.co>
2020-12-25 18:40:21 +0100

    tests work

---
+++ backend/test/auth.js
@@ -0,0 +1,34 @@
+const { post } = require('./util');
+const chai = require('chai');
+const should = chai.should();
+
+const sampleUser = {
+    name: {
+        first: "John",
+        middle: "Joseph",
+        last: "Doe",
+    },
+    email: "john.doe@mail.com",
+    password: "johndoepassword",
+    weight: 75.3,
+    birthDate: new Date(),
+    gender: "male"
+};
+
+describe('Auth', () => {
+    describe('/POST register', () => {
+        it('it should get 400 status with no body', (done) => {
+            post('/auth/register', {}, (err, res) => {
+                res.should.have.status(400);
+                done();
+            })
+        });
+
+        it('it should get 200 status with correctly formed body', (done) => {
+            post('/auth/register', sampleUser, (err, res) => {
+                res.should.have.status(200);
+                done();
+            })
+        });
+    });
+});
