49f73ede0af61b81bf33262a0fd58f4be1f401e1
DaVinciTachyon <davincitachyon@gmail.com>
2021-05-06 01:10:50 +0100

    more refs

--- website/src/components/Workouts/WorkoutTracker.js
+++ website/src/components/Workouts/WorkoutTracker.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Exercises from "./Exercises";
 
 export default class WorkoutTracker extends React.Component {
   constructor() {
@@ -9,7 +8,10 @@ export default class WorkoutTracker extends React.Component {
 
   render() {
     return (
-      <Exercises />
+      <div>
+        <div>log workout</div>
+        <div>workout history</div>
+      </div>
     );
   }
 }
