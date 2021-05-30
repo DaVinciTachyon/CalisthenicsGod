29759fa10b6c6ab6bccb0bdca88c8b8c68c27af9
DaVinciTachyon <davincitachyon@gmail.com>
2021-05-30 18:37:20 +0100

    -y

--- setUp.sh
+++ setUp.sh
@@ -1,14 +1,14 @@
 # !/bin/bash
 
-sudo apt-get remove docker docker-engine docker.io containerd runc docker-compose
+sudo apt-get remove -y docker docker-engine docker.io containerd runc docker-compose
 sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
 
 sudo apt-get update
 sudo apt-get install -y nodejs npm
