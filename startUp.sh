9978412b4a4921fa2d624f1e9519755ed8746e5e
DaVinciTachyon <davincitachyon@gmail.com>
2021-06-01 00:15:02 +0100

    ugly

--- startUp.sh
+++ startUp.sh
@@ -3,19 +3,18 @@
 # ./setUp.sh
 
 if [ -n "$1" ] && [ "$1" = "production" ]; then
-    env=.production.local
-    options=-d
+    sudo docker-compose --env-file ./config/.env.production.local build
+    sudo docker-compose --env-file ./config/.env.production.local up -d
 elif [ -n "$1" ] && [ "$1" = "test" ]; then
-    env=.test.local
-    if [ -n "$2" ]; then
-        options=`-d $2`
+    sudo docker-compose --env-file ./config/.env.test.local build
+    if [ -n "$2" ] && [ "$2" = "backend" ]; then
+        sudo docker-compose --env-file ./config/.env.test.local up -d backend
+    elif [ -n "$2" ] && [ "$2" = "website" ]; then
+        sudo docker-compose --env-file ./config/.env.test.local up -d website
     else
-        options=-d
+        sudo docker-compose --env-file ./config/.env.test.local up -d
     fi
 else
-    env=.development.local
-    options=backend
+    sudo docker-compose --env-file ./config/.env.development.local build
+    sudo docker-compose --env-file ./config/.env.development.local up backend
 fi
-
-sudo docker-compose --env-file ./config/.env$env build
-sudo docker-compose --env-file ./config/.env$env up $options
