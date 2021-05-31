7d7ca40980dc9935cdd17023bedff30998b7daec
DaVinciTachyon <davincitachyon@gmail.com>
2021-05-31 21:49:13 +0100

    expose

--- startUp.sh
+++ startUp.sh
@@ -4,9 +4,12 @@
 
 sudo docker-compose build
 if [ -n "$1" ] && [ "$1" = "production" ]; then
+    echo hi
     sudo docker-compose --env-file ./config/.env.production.local up -d
 elif [ -n "$1" ] && [ "$1" = "test" ]; then
+    echo ello
     sudo docker-compose --env-file ./config/.env.test.local up -d backend
 else
+    echo bibi
     sudo docker-compose --env-file ./config/.env.development.local up backend
 fi
