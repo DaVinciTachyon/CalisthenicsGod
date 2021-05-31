c863e24493075a08fa4451ac6b20f585c2c2515a
DaVinciTachyon <davincitachyon@gmail.com>
2021-05-31 21:19:43 +0100

    test env

--- startUp.sh
+++ startUp.sh
@@ -6,7 +6,7 @@ sudo docker-compose build
 if [ -n "$1" ] && [ "$1" = "production" ]; then
     sudo docker-compose --env-file ./config/.env.production.local up -d
 elif [ -n "$1" ] && [ "$1" = "test" ]; then
-    sudo docker-compose --env-file ./config/.env.test.local up -d
+    sudo docker-compose --env-file ./config/.env.test.local up -d backend
 else
-    sudo docker-compose --env-file ./config/.env.development.local up
+    sudo docker-compose --env-file ./config/.env.development.local up backend
 fi
