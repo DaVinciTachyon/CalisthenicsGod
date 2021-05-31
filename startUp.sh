fb8b61d9f14eee7088b4754e439b0909d8a50d54
DaVinciTachyon <davincitachyon@gmail.com>
2021-05-31 19:44:25 +0100

    start up

---
+++ startUp.sh
@@ -0,0 +1,13 @@
+# !/bin/bash
+
+./setUp
+
+if [$1 = 'production']
+then
+    sudo docker-compose up --env-file ./config/.env.production.local -d --build
+elif [$1 = 'test']
+then
+    sudo docker-compose up --env-file ./config/.env.test.local -d --build
+else
+    sudo docker-compose up --env-file ./config/.env.development.local --build
+fi
