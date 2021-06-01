d96cd9445529c2368dc553fcb7a28ab146d8d8fa
DaVinciTachyon <davincitachyon@gmail.com>
2021-06-01 21:54:47 +0100

    added files

---
+++ backend/test/presetMeals.js
@@ -0,0 +1,17 @@
+const { get, login } = require('./util');
+const chai = require('chai');
+const should = chai.should();
+let authToken = '';
+
+before(async () => {
+  authToken = await login();
+});
+
+describe('Preset Meals', () => {
+  //   describe('/GET', () => {
+  //     it('valid request', async () => {
+  //       const res = await get('/api/nutrition/ingredients', authToken);
+  //       res.should.have.status(200);
+  //     });
+  //   });
+});
