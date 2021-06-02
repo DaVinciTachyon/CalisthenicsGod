dcaf5046166b2df47c3dfa02a4f3da5ef1415c17
DaVinciTachyon <davincitachyon@gmail.com>
2021-06-02 01:33:29 +0100

    nutrition

--- backend/test/nutrition.js
+++ backend/test/nutrition.js
@@ -1,4 +1,4 @@
-const { get, login } = require('./util');
+const { get, login, post, randomInt, randomFloat } = require('./util');
 const chai = require('chai');
 const should = chai.should();
 let authToken = '';
@@ -8,11 +8,45 @@ before(async () => {
 });
 
 describe('Nutrition', () => {
-  //TODO
-  //   describe('/GET', () => {
-  //     it('valid request', async () => {
-  //       const res = await get('/api/nutrition/ingredients', authToken);
-  //       res.should.have.status(200);
-  //     });
-  //   });
+  describe('/GET', () => {
+    it('valid request', async () => {
+      const res = await get('/api/nutrition', authToken);
+      res.should.have.status(200);
+    });
+  });
+
+  describe('/POST', () => {
+    it('empty body', async () => {
+      const res = await post('/api/nutrition', {}, authToken);
+      res.should.have.status(400);
+    });
+
+    it('valid nutrient info', async () => {
+      const res = await post(
+        '/api/nutrition',
+        {
+          calorieOffset: randomInt(),
+          caloriesPerKg: randomInt(),
+          proteinGramsPerKg: randomFloat(),
+          fatCalorieProportion: randomFloat(0, 1),
+        },
+        authToken
+      );
+      res.should.have.status(200);
+    });
+  });
+
+  describe('/GET /goals', () => {
+    it('valid request', async () => {
+      const res = await get('/api/nutrition/goals', authToken);
+      res.should.have.status(200);
+    });
+  });
+
+  describe('/GET /macronutrientDensities', () => {
+    it('valid request', async () => {
+      const res = await get('/api/nutrition/macronutrientDensities', authToken);
+      res.should.have.status(200);
+    });
+  });
 });
