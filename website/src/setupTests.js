dfab1f5ae3802add92bc72d916a6946dc45e7208
DaVinciTachyon <davincitachyon@gmail.com>
2021-06-08 15:42:24 +0100

    port

--- website/src/setupTests.js
+++ website/src/setupTests.js
@@ -6,7 +6,7 @@ import '@testing-library/jest-dom/extend-expect';
 import puppeteer from 'puppeteer';
 
 jest.setTimeout(10000);
-global.url = process.env.REACT_APP_WEBSITE_URL || 'http://localhost:3000';
+global.url = process.env.REACT_APP_WEBSITE_URL || 'http://localhost:5000';
 
 beforeAll(async () => {
   global.browser = await puppeteer.launch({});
