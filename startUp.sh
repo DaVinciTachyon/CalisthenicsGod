8bd7357ff5e12ec38370e67f201881f3e6969f20
DaVinciTachyon <davincitachyon@gmail.com>
2021-06-12 01:14:06 +0100

    setting

--- startUp.sh
+++ startUp.sh
@@ -6,12 +6,14 @@ if [ -n "$1" ] && [ "$1" = "production" ]; then
     sudo docker-compose --env-file ./config/.env.production build caddy website backend
     sudo docker-compose --env-file ./config/.env.production up -d caddy
 elif [ -n "$1" ] && [ "$1" = "test" ]; then
-    sudo docker-compose --env-file ./config/.env.test build
     if [ -n "$2" ] && [ "$2" = "backend" ]; then
+        sudo docker-compose --env-file ./config/.env.test build backend calisthenicsgodb
         sudo docker-compose --env-file ./config/.env.test up -d backend calisthenicsgodb
     elif [ -n "$2" ] && [ "$2" = "website" ]; then
+        sudo docker-compose --env-file ./config/.env.test build website backend calisthenicsgodb
         sudo docker-compose --env-file ./config/.env.test up -d website backend calisthenicsgodb
     else
+        sudo docker-compose --env-file ./config/.env.test build caddy website backend calisthenicsgodb
         sudo docker-compose --env-file ./config/.env.test up -d caddy website backend calisthenicsgodb
     fi
 fi
