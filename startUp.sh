9186ce96c6228375ccb944046e64a94748ba481f
DaVinciTachyon <davincitachyon@gmail.com>
2021-06-28 19:44:38 +0100

    push

--- startUp.sh
+++ startUp.sh
@@ -3,7 +3,10 @@
 # ./setUp.sh
 
 if [ -n "$1" ] && [ "$1" = "production" ]; then
+    git pull
     sudo docker-compose --env-file ./config/.env.production build caddy website backend
+    sudo docker-compose down
+    sudo docker system prune -f
     sudo docker-compose --env-file ./config/.env.production up -d caddy
 elif [ -n "$1" ] && [ "$1" = "test" ]; then
     if [ -n "$2" ] && [ "$2" = "backend" ]; then
